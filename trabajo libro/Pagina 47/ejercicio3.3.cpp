288e245da30f57920df17a1b91cead7afc352a4d
Erikaapaza19 <135631401+Erikaapaza19@users.noreply.github.com>
2023-07-17 17:30:45 -0500

    Update ejercicio3.3.cpp

--- trabajo libro/Pagina 47/ejercicio3.3.cpp
+++ trabajo libro/Pagina 47/ejercicio3.3.cpp
@@ -3,21 +3,16 @@
 using namespace std;
 int main(){
 float a, b, c, d, e, f, denominador, x, y;
-cout << " Introduzca el valor de a de b y de c ";
-cin >> a, b, c;
-cout << " Introduzca el valor de d de e y de f " ;
 cin >> d >> e >> f;
 denominador = (a * e);
 denominador = (b * d);
 denominador = ((a * e) - (b * d));
 if (denominador == 0)
 cout << " no solucion\n";
-else {
-x = (c * e - b * f) / denominador;
-y = ((a * f - c * d)*2) / denominador;
+else {{}}
+
 cout << " la solucion del sistema es\n";
 cout << " x = " << x << " y = " << y << endl;
 }
 
 return 0;
-} // MODIFICAR EL EJERCICIO PARA EL CASO QUE SI EL RESULTADO DE "X" ES POSITIVO, EL RESULTADO DE "Y", DEBERA SER ELEVADO AL CUADRADO, CASO CONTRARIO SE LE RESTARA 2
